9ad452bb795f6ea1d56dd04f039da53613d5549c
AaronRDZGZZ <a.a.r.g@hotmail.com>
2024-02-06 02:53:20 -0600

    end of section 3

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,4 +1,5 @@
 
+using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,16 @@ namespace Infrastructure.Data
         }
 
         public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductBrand> ProductBrand { get; set; }
+
+        public DbSet<ProductType> ProductType { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
     }
 }
